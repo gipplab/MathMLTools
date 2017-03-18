4d424b4156c3514898df2be4449671dec1fcf593
Moritz Schubotz <wiki@physikerwelt.de>
2017-03-18 18:41:44 +0100

    Adjust whitespaces

--- src/main/java/com/formulasearchengine/mathmlquerygenerator/NtcirTopicReader.java
+++ src/main/java/com/formulasearchengine/mathmlquerygenerator/NtcirTopicReader.java
@@ -24,83 +24,85 @@ import static com.formulasearchengine.mathmltools.xmlhelper.NonWhitespaceNodeLis
  * http://ntcir-math.nii.ac.jp/wp-content/blogs.dir/13/files/2014/05/NTCIR11-Math-topics.pdf
  */
 public class NtcirTopicReader {
-	public static final String NS_NII = "http://ntcir-math.nii.ac.jp/";
-	private final Document topics;
-	private final List<NtcirPattern> patterns = new ArrayList<>();
-	private final XQueryGenerator queryGenerator;
+    public static final String NS_NII = "http://ntcir-math.nii.ac.jp/";
+    private final Document topics;
+    private final List<NtcirPattern> patterns = new ArrayList<>();
+    private final XQueryGenerator queryGenerator;
 
-	public NtcirTopicReader( Document topics ) {
-		this.topics = topics;
-		queryGenerator = new XQueryGenerator( topics );
-	}
+    public NtcirTopicReader(Document topics) {
+        this.topics = topics;
+        queryGenerator = new XQueryGenerator(topics);
+    }
 
-	public NtcirTopicReader( File topicFile ) throws ParserConfigurationException, IOException, SAXException {
-		DocumentBuilder documentBuilder = XMLHelper.getDocumentBuilder( true );
-		topics = documentBuilder.parse( topicFile );
+    public NtcirTopicReader(File topicFile) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilder documentBuilder = XMLHelper.getDocumentBuilder(true);
+        topics = documentBuilder.parse(topicFile);
 
-		//TODO: Find out how this code duplication can be avoided in Java.
-		queryGenerator = new XQueryGenerator( topics );
-	}
+        //TODO: Find out how this code duplication can be avoided in Java.
+        queryGenerator = new XQueryGenerator(topics);
+    }
 
-	public NtcirTopicReader( Document topics, String namespace, String pathToRoot, String returnFormat, boolean restrictLength ) {
-		queryGenerator = new XQueryGenerator( topics );
-		this.topics = topics;
-		this.setNamespace(namespace).setReturnFormat(returnFormat).setPathToRoot(pathToRoot).setRestrictLength(restrictLength);
-	}
+    public NtcirTopicReader(Document topics, String namespace, String pathToRoot, String returnFormat, boolean restrictLength) {
+        queryGenerator = new XQueryGenerator(topics);
+        this.topics = topics;
+        this.setNamespace(namespace).setReturnFormat(returnFormat).setPathToRoot(pathToRoot).setRestrictLength(restrictLength);
+    }
 
-	public final NtcirTopicReader setReturnFormat(String returnFormat) {
-		queryGenerator.setReturnFormat(returnFormat);
-		return this;
-	}
+    public final NtcirTopicReader setReturnFormat(String returnFormat) {
+        queryGenerator.setReturnFormat(returnFormat);
+        return this;
+    }
 
-	public final NtcirTopicReader setNamespace(String namespace) {
-		queryGenerator.setNamespace(namespace);
-		return this;
-	}
+    public final NtcirTopicReader setNamespace(String namespace) {
+        queryGenerator.setNamespace(namespace);
+        return this;
+    }
 
-	public final NtcirTopicReader setPathToRoot(String pathToRoot) {
-		queryGenerator.setPathToRoot(pathToRoot);
-		return this;
-	}
+    public final NtcirTopicReader setPathToRoot(String pathToRoot) {
+        queryGenerator.setPathToRoot(pathToRoot);
+        return this;
+    }
 
-	public final NtcirTopicReader setFindRootApply(boolean findRootApply) {
-		queryGenerator.setFindRootApply(findRootApply);
-		return this;
-	}
+    public final NtcirTopicReader setFindRootApply(boolean findRootApply) {
+        queryGenerator.setFindRootApply(findRootApply);
+        return this;
+    }
 
-	public final NtcirTopicReader setRestrictLength( boolean restrictLength ) {
-		queryGenerator.setRestrictLength( restrictLength );
-		return this;
-	}
+    public final NtcirTopicReader setRestrictLength(boolean restrictLength) {
+        queryGenerator.setRestrictLength(restrictLength);
+        return this;
+    }
 
-	public final NtcirTopicReader setAddQvarMap( boolean addQvarMap ) {
-		queryGenerator.setAddQvarMap( addQvarMap );
-		return this;
-	}
+    public final NtcirTopicReader setAddQvarMap(boolean addQvarMap) {
+        queryGenerator.setAddQvarMap(addQvarMap);
+        return this;
+    }
 
-	/**
-	 * Splits the given NTCIR query file into individual queries, converts each query into an XQuery using
-	 * XQueryGenerator, and returns the result as a list of NtcirPatterns for each individual query.
-	 * @return List of NtcirPatterns for each query
-	 * @throws XPathExpressionException Thrown if xpaths fail to compile or fail to evaluate
-	 +	 */
-	public final List<NtcirPattern> extractPatterns() throws XPathExpressionException {
-		final XPath xpath = XMLHelper.namespaceAwareXpath( "t", NS_NII );
-		final XPathExpression xNum = xpath.compile( "./t:num" );
-		final XPathExpression xFormula = xpath.compile( "./t:query/t:formula" );
-		final NonWhitespaceNodeList topicList = new NonWhitespaceNodeList(
-			topics.getElementsByTagNameNS( NS_NII, "topic" ) );
-		for ( final Node node : topicList ) {
-			final String num = xNum.evaluate( node );
-			final NonWhitespaceNodeList formulae = new NonWhitespaceNodeList( (NodeList)
-				xFormula.evaluate( node, XPathConstants.NODESET ) );
-			for ( final Node formula : formulae ) {
-				final String id = formula.getAttributes().getNamedItem( "id" ).getTextContent();
-				final Node mathMLNode = getFirstChild( formula );
-				queryGenerator.setMainElement( getFirstChild( mathMLNode ) );
-				patterns.add( new NtcirPattern( num, id, queryGenerator.toString(), mathMLNode ) );
-			}
-		}
-		return patterns;
-	}
+    /**
+     * Splits the given NTCIR query file into individual queries, converts each query into an XQuery using
+     * XQueryGenerator, and returns the result as a list of NtcirPatterns for each individual query.
+     *
+     * @return List of NtcirPatterns for each query
+     * @throws XPathExpressionException Thrown if xpaths fail to compile or fail to evaluate
+     *                                  +
+     */
+    public final List<NtcirPattern> extractPatterns() throws XPathExpressionException {
+        final XPath xpath = XMLHelper.namespaceAwareXpath("t", NS_NII);
+        final XPathExpression xNum = xpath.compile("./t:num");
+        final XPathExpression xFormula = xpath.compile("./t:query/t:formula");
+        final NonWhitespaceNodeList topicList = new NonWhitespaceNodeList(
+                topics.getElementsByTagNameNS(NS_NII, "topic"));
+        for (final Node node : topicList) {
+            final String num = xNum.evaluate(node);
+            final NonWhitespaceNodeList formulae = new NonWhitespaceNodeList((NodeList)
+                    xFormula.evaluate(node, XPathConstants.NODESET));
+            for (final Node formula : formulae) {
+                final String id = formula.getAttributes().getNamedItem("id").getTextContent();
+                final Node mathMLNode = getFirstChild(formula);
+                queryGenerator.setMainElement(getFirstChild(mathMLNode));
+                patterns.add(new NtcirPattern(num, id, queryGenerator.toString(), mathMLNode));
+            }
+        }
+        return patterns;
+    }
 }
