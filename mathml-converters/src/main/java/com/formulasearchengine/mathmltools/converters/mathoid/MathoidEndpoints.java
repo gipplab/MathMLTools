22643789618e49ef323a1daf0d7e858e3fba1258
AndreG-P <andre.greiner-petter@t-online.de>
2018-04-24 02:37:31 +0900

    Add MediaTypes to MathoidEndpoints

--- mathml-converters/src/main/java/com/formulasearchengine/mathmltools/converters/mathoid/MathoidEndpoints.java
+++ mathml-converters/src/main/java/com/formulasearchengine/mathmltools/converters/mathoid/MathoidEndpoints.java
@@ -1,21 +1,33 @@
 package com.formulasearchengine.mathmltools.converters.mathoid;
 
+import com.google.common.net.MediaType;
+
 /**
  * @author Andre Greiner-Petter
  */
 public enum MathoidEndpoints {
-    SVG_ENDPOINT("svg"),
-    PNG_ENDPOINT("png"),
-    MML_ENDPOINT("mml"),
-    INFO_ENDPOINT("texvcinfo");
+    SVG_ENDPOINT("svg", MediaType.SVG_UTF_8),
+    PNG_ENDPOINT("png", MediaType.PNG),
+    MML_ENDPOINT("mml", MediaType.create("application", "mathml+xml")),
+    INFO_ENDPOINT("texvcinfo", MediaType.JSON_UTF_8);
 
     private final String endpoint;
+    private final MediaType responseMediaType;
 
-    MathoidEndpoints(String endpoint) {
+    MathoidEndpoints(String endpoint, MediaType responseMediaType) {
         this.endpoint = endpoint;
+        this.responseMediaType = responseMediaType;
     }
 
     public String getEndpoint(String host) {
         return host + "/" + endpoint;
+    }
+
+    public MediaType getResponseMediaType() {
+        return responseMediaType;
+    }
+
+    public static void main(String[] args){
+        System.out.println(MML_ENDPOINT.getResponseMediaType().toString());
     }
 }
